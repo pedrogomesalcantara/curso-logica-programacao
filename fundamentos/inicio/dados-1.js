00f00342a5a1fae329f86d64db834fbddaec8326
Pedro Gomes Alcantara <pedroalcantara901@outlook.com>
2023-03-13 20:04:21 -0300

    create ex dado-1.js

---
+++ fundamentos/inicio/dados-1.js
@@ -0,0 +1,18 @@
+var quantidade = 10; // VARIAVEL - "VAR", ARMAZENA UM 'DADO' EM UMA 'CAIXA' QUE PODE SER DE QUALQUER NOME. ESSE DADO PODE SER PUXADO NOVAMENTE EM QUALQUER PARTE DO CÓDIGO.
+
+var nome = "caneta";
+
+var preco = 6.4;
+
+var imposto = 1.5;
+
+var precoFinal = preco + imposto;
+
+console.log(nome);
+console.log(quantidade)   //QUANDO COLOCADO UM VALOR DIRETO, OU UMA PALAVRA/FRASE QUE SEJE DIRETA, SE CHAMA DE VALOR "LITERAL"
+console.log(preco)
+console.log(imposto)
+console.log (precoFinal);
+
+nome = "Caneta Bic"
+console.log (nome);
