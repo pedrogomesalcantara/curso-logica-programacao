38bb64350b48988df4e8e850762693badbb07692
Pedro Gomes Alcantara <pedroalcantara901@outlook.com>
2023-03-15 20:48:14 -0300

    att ex dados-3, criado ex dados-4

---
+++ fundamentos/inicio/dados-3.js
@@ -0,0 +1,21 @@
+let idade = 31;
+console.log(typeof 31);
+console.log(typeof idade);
+
+let salario = 4578.32;  // ESSE PONTO É CHAMADO DE PONTO "FLUTUANTE" 
+
+// NEM TODAS AS LINGUAGENS TRATA OS NUMEROS INTEIROS E NUMEROS QUEBRADOS DA MESMA FORMA.
+
+console.log(typeof salario);
+
+let estaChovendo = true // FALSE; 
+// VALOR BOOLEAN/BOOLEANO.
+
+console.log(typeof estaChovendo);
+
+
+console.log(typeof "teste"); 
+
+console.log(typeof 'Teste');
+
+// "STRING" DEFINIDO POR ASPAS "" DUPLOS, OU ASPAS '' SIMPLES.
