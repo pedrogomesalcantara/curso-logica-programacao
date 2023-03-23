f54dc16d20c1d043e867fc96f3d8f0d8001b3a41
Pedro Gomes Alcantara <pedroalcantara901@outlook.com>
2023-03-22 21:07:01 -0300

    att

---
+++ fundamentos/operadores/desafio_aritimeticos.js
@@ -0,0 +1,25 @@
+//FORMULA PARA DESCOBRIR RESULTADO DE "C"
+
+let f = 73;
+let num = 32;
+let div = 9;
+let divisao = 5;
+
+console.log((f - num ) / div * divisao);
+
+
+/*      RESPOSTA
+
+
+let f = 73;
+
+let c = (f - 32) / 9 * 5;
+
+console.log(c);
+
+
+*/
+
+
+
+
