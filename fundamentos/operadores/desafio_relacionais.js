2046514514778b721a75e0f4d86e05617df70196
Pedro Gomes Alcantara <pedroalcantara901@outlook.com>
2023-03-25 21:43:43 -0300

    att

---
+++ fundamentos/operadores/desafio_relacionais.js
@@ -0,0 +1,24 @@
+// encontre 5 expressões que o resultado é FALSE;
+
+// encontre 5 expressões que o resultado é TRUE;
+
+let a = 1;
+let b = 2;
+
+
+// RESULTADO - FALSE -
+
+console.log(a > b);
+console.log(b === a);
+console.log(b == a);
+console.log(a >= b);
+console.log(b <= a);
+
+
+// RESULTADO - TRUE -
+
+console.log(a != b);
+console.log(a < b);
+console.log(b > a);
+console.log(a !== b);
+console.log(b >= a)
