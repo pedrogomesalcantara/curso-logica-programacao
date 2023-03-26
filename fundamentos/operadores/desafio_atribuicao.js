2af697440f942b09e3330b111a7e5e335984d1b9
Pedro Gomes Alcantara <pedroalcantara901@outlook.com>
2023-03-25 23:08:17 -0300

    att

---
+++ fundamentos/operadores/desafio_atribuicao.js
@@ -0,0 +1,28 @@
+//let texto = 'Bom dia! \n Tudo bem?';
+// texto += '/n Tudo Bem!'
+//console.log(texto);
+
+/*
+Lista de Aprovados
+==================
+1. Ana Silva
+2. Pedro Abuquerque
+3. Guilherme Pereira
+4. Rebeca França
+
+
+*/
+
+//let texto = 'Lista de Aprovados\n\==================\n \n1. Ana Silva\n2. Pedro Abuquerque\n3. Guilherme Pereira\n4. Rebeca França ';
+
+//console.log(texto);
+
+let texto = 'Lista de Aprovados\n\n';
+
+texto += '===============\n\n';
+texto += '1. Ana Silva\n';
+texto += '2. Pedro Abuquerque\n';
+texto += '3. Guilherme Pereira\n';
+texto += '4. Rebeca França\n';
+
+console.log(texto);
