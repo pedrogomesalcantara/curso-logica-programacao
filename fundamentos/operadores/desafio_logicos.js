b7a714d0069d26817e766c43a1db9db10242ac43
Pedro Gomes Alcantara <pedroalcantara901@outlook.com>
2023-03-27 22:38:12 -0300

    att

--- fundamentos/operadores/desafio_logicos.js
+++ fundamentos/operadores/desafio_logicos.js
@@ -1,7 +1,7 @@
 // Duas expressões resultar em TRUE - ARITMETICOS, RELACIONAIS, LOGICOS -;
 
-let verdadeiro1 = ?;
-let verdadeiro2 = ?;
+let verdadeiro1 
+let verdadeiro2 =
 
 console.log(verdadeiro1);
 console.log(verdadeiro2);
@@ -10,8 +10,8 @@ console.log(verdadeiro2);
 
 //Duas expressões em FALSE - ARITMÉTICOS, RELACIONAIS, LÓGICOS
 
-let falso1 = ??;
-let falso2 = ??;
+let falso1  
+let falso2 =
 
 console.log(falso1);
 console.log(falso2);
