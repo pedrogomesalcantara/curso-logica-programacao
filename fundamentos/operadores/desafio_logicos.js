b442dbf8873880ed307fa95301d4f68c4bfcefb1
Pedro Gomes Alcantara <pedroalcantara901@outlook.com>
2023-04-03 22:09:31 -0300

    att

--- fundamentos/operadores/desafio_logicos.js
+++ fundamentos/operadores/desafio_logicos.js
@@ -1,7 +1,7 @@
 // Duas expressões resultar em TRUE - ARITMETICOS, RELACIONAIS, LOGICOS -;
 
-let verdadeiro1 
-let verdadeiro2 =
+let verdadeiro1 = 2 + 5 >= 7 && 8 > 2;
+let verdadeiro2 = !(6 * 6 < 3);
 
 console.log(verdadeiro1);
 console.log(verdadeiro2);
@@ -10,8 +10,8 @@ console.log(verdadeiro2);
 
 //Duas expressões em FALSE - ARITMÉTICOS, RELACIONAIS, LÓGICOS
 
-let falso1  
-let falso2 =
+let falso1 = 8 - 4 > 12 || false || 15 === '15';
+let falso2 = !(7 !== 6 + 1 || '3' !== 3);
 
 console.log(falso1);
 console.log(falso2);
---
+++ fundamentos/operadores/unarios_1.js
@@ -0,0 +1,12 @@
+let a = 1;
+
+a = a + 1; // 'IN'FIX
+
+a += 1; // 'POST'FIX
+
+a ++; //OPERADOR UNARIO "OPERA EM CIMA DE UM OPERANDO"
+++a; // 'PRE'FIX
+
+a--;
+--a;
+console.log(a);
