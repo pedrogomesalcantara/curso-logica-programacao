a4ea2e607e0178ae31497dcbb4a24c952d1bc15a
Pedro Gomes Alcantara <pedroalcantara901@outlook.com>
2023-04-06 22:21:29 -0300

    att

---
+++ fundamentos/operadores/ternario_1.js
@@ -0,0 +1,3 @@
+const hora =16;
+const saudacao = hora <= 11 ? 'Bom Dia' : 'Boa Tarde' ;
+console.log(saudacao);
---
+++ fundamentos/operadores/ternario_2.js
@@ -0,0 +1,4 @@
+const hora = 9;
+const saudacao = hora <= 11 ? "Bom Dia" : hora <= 17 ? "Boa Tarde" : "Boa Noite"
+
+console.log(saudacao);
