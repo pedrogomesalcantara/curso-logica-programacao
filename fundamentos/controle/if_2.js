88734ef410904ee78f2a8a2bd0d47bf2dfe2cfc7
Pedro Gomes Alcantara <pedroalcantara901@outlook.com>
2023-04-11 20:58:18 -0300

    att

---
+++ fundamentos/controle/if_2.js
@@ -0,0 +1,11 @@
+const nota = 8;
+const bomComportamento = false;
+
+
+if (nota >= 9 && bomComportamento)
+    console.log('parabens');
+
+if (nota < 7 || !bomComportamento)
+    console.log('Uma pena');
+
+console.log('fim')
---
+++ fundamentos/controle/if_3.js
@@ -0,0 +1,10 @@
+const nota = 9.3 ;
+
+if (nota >= 9)  {
+
+    console.log('Quadro de honra');
+    console.log('Parabens');
+}   
+
+console.log('fim');
+    
