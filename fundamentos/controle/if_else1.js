f2b2b9953694534497782fa97736009bde2313d2
Pedro Gomes Alcantara <pedroalcantara901@outlook.com>
2023-06-05 19:52:01 -0300

    ATT

---
+++ fundamentos/controle/if_else1.js
@@ -0,0 +1,19 @@
+const verdadeira = true;
+const falsa = false;
+
+
+if(verdadeira)
+    console.log('Exec...');
+else {
+    console.log('Não exec...')
+
+}
+
+if (falsa)  {
+    console.log('Não exec..');
+    
+}
+else
+    console.log('Exec...');
+// PODE MISTURAR ENTRE SENTENÇA DE CODIGO E BLOCO DE CÓDIGO SEM PROBLEMAS.
+//--> POR 'ESTILO' SEMPRE É COLOCADO ENTRE "CHAVES" TANTO O IF QUANTO O ELSE.
