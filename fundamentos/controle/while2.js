84f11ce8d95d3c4a9d3b9087466c390d45e5c8d3
Pedro Gomes Alcantara <pedroalcantara901@outlook.com>
2023-06-13 20:55:25 -0300

    att

---
+++ fundamentos/controle/while2.js
@@ -0,0 +1,10 @@
+let quantidade = 0;
+
+//QUANTIDADE INDETERMINADA DE REPETIÇOES..
+
+while(Math.random() < 0.8) {
+    console.log('< 0.9');
+    quantidade++;
+}
+
+console.log('QTDE' + quantidade);
