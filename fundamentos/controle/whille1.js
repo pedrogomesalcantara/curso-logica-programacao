24014ffd587d3610130b04700d6806ab8f3e0384
Pedro Gomes Alcantara <pedroalcantara901@outlook.com>
2023-06-13 20:23:04 -0300

    att

---
+++ fundamentos/controle/whille1.js
@@ -0,0 +1,11 @@
+//CONSOLE.LOG('EXEC...'     Ctrl + Alt + N);
+//CONSOLE.LOG('PARAR...'    Ctrl + Alt + M);
+
+while (false) {
+    console.log( 'Laço Infinito!!!')
+}
+
+console.log('fim');
+
+//QUANDO A EXPRESSÃO FOR 'TRUE' SERÁ UM LAÇO INFINTO
+// QUANDO A EXPRESSÃO FOR 'FALSE' CHEGARA NO FIM DO BLOCO.
