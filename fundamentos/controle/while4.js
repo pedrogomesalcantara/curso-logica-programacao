783b47de2c3ea326efaa1a5f9b96ac47a06fe55c
Pedro Gomes Alcantara <pedroalcantara901@outlook.com>
2023-06-13 21:30:15 -0300

    ATT

---
+++ fundamentos/controle/while4.js
@@ -0,0 +1,10 @@
+let controle = 1;
+
+while(controle <= 10) {
+    console.log(controle);
+    controle++;
+}
+
+console.log('Fim');
+
+//declaraçao. expressão. incremento; -> qtde determinada de repetições.
