ca89e220777a9c5662a85932efdb07391c2c117c
Pedro Gomes Alcantara <pedroalcantara901@outlook.com>
2023-09-23 20:16:33 -0300

    att

--- fundamentos/funcoes/sem-funcao.js
+++ fundamentos/funcoes/sem-funcao.js
@@ -5,3 +5,12 @@ const a1 = 2021;
 console.log(`Dia: ${d1}`);
 console.log(`Mês: ${m1}`);
 console.log(`Ano: ${a1}`);
+
+
+const d2 = 23;
+const m2 = 12;
+const a2 = 2022;
+
+console.log(`Dia: ${d2}`);
+console.log(`Mês: ${m2}`);
+console.log(`Ano: ${a2}`);
