328583a20d4d99d870442455c9165291240f251b
Pedro Gomes Alcantara <pedroalcantara901@outlook.com>
2023-09-24 21:53:08 -0300

    funcao

---
+++ fundamentos/funcoes/desafio-1-funcao.js
@@ -0,0 +1,32 @@
+// funcao: 2 parametros -> console.log(+ somar)
+// funcao: 2 parametros -> console.log(- sub)
+
+const som1 = 3;
+const som2 = 4;
+const som3 = 31;
+const som4 = 98;
+
+const sub1 = 11;
+const sub2 = 7;
+const sub3 = 19;
+const sub4 = 52;
+
+
+function somar (n1, n4) {
+
+    console.log(n1 + n4);
+
+};
+
+somar(som1, som4);
+somar(som2, som3);
+
+
+function subtrair (n1, n2) {
+
+    console.log(n1 - n2);
+
+}
+
+subtrair(sub1, sub2);
+subtrair(sub3, sub4);
